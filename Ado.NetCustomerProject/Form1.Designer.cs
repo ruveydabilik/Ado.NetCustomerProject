146ee9e77c3b693f202819e0ee8caea5abf94bcc
Ayşe Rüveyda Bilik <121570981+ruveydabilik@users.noreply.github.com>
2024-11-10 01:44:30 +0300

    Form is created and listing the cities is done

--- Ado.NetCustomerProject/Form1.Designer.cs
+++ Ado.NetCustomerProject/Form1.Designer.cs
@@ -28,13 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtCityCountry = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCityName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnList = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtCityId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.DarkSalmon;
+            this.MainPanel.Controls.Add(this.btnSearch);
+            this.MainPanel.Controls.Add(this.btnUpdate);
+            this.MainPanel.Controls.Add(this.btnDelete);
+            this.MainPanel.Controls.Add(this.btnCreate);
+            this.MainPanel.Controls.Add(this.txtCityCountry);
+            this.MainPanel.Controls.Add(this.label3);
+            this.MainPanel.Controls.Add(this.txtCityName);
+            this.MainPanel.Controls.Add(this.label2);
+            this.MainPanel.Controls.Add(this.radioButton1);
+            this.MainPanel.Controls.Add(this.btnList);
+            this.MainPanel.Controls.Add(this.dataGridView);
+            this.MainPanel.Controls.Add(this.txtCityId);
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Location = new System.Drawing.Point(-1, -3);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1147, 429);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(136, 295);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(117, 44);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Ara";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(212, 232);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(117, 44);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(212, 173);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(117, 44);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(64, 232);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(117, 44);
+            this.btnCreate.TabIndex = 9;
+            this.btnCreate.Text = "Ekle";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtCityCountry
+            // 
+            this.txtCityCountry.Location = new System.Drawing.Point(145, 123);
+            this.txtCityCountry.Name = "txtCityCountry";
+            this.txtCityCountry.Size = new System.Drawing.Size(184, 22);
+            this.txtCityCountry.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(96, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ülke:";
+            // 
+            // txtCityName
+            // 
+            this.txtCityName.Location = new System.Drawing.Point(145, 79);
+            this.txtCityName.Name = "txtCityName";
+            this.txtCityName.Size = new System.Drawing.Size(184, 22);
+            this.txtCityName.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(70, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Şehir Adı:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(31, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(64, 173);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(117, 44);
+            this.btnList.TabIndex = 3;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(376, 37);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(726, 312);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // txtCityId
+            // 
+            this.txtCityId.Location = new System.Drawing.Point(145, 37);
+            this.txtCityId.Name = "txtCityId";
+            this.txtCityId.Size = new System.Drawing.Size(184, 22);
+            this.txtCityId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(79, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehir Id:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1149, 426);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "Form1";
+            this.Text = "Şehir Bilgi Formu";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtCityId;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtCityCountry;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCityName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
---
+++ Ado.NetCustomerProject/Utilities/ConfigurationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+
+namespace Ado.NetCustomerProject.Utilities
+{
+    public static class ConfigurationHelper
+    {
+        // We create a static field to hold the connection string.
+        public static readonly string ConnectionString;
+
+        static ConfigurationHelper()
+        {
+            try
+            {
+                var configFileMap = new ExeConfigurationFileMap { ExeConfigFilename = "connection.config" };
+                var config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                ConnectionString = config.ConnectionStrings.ConnectionStrings["MyDatabase"]?.ConnectionString;
+
+                if (ConnectionString == null)
+                {
+                    throw new Exception("Connection string 'MyDatabase' bulunamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata: {ex.Message}");
+                throw; // If you want to continue throwing the error and see where it occurs
+            }
+        }
+    }
+}
